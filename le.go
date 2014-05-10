2198ad93569e9dea285926f0b4321dc62d98e1d8
Gal Ben-Haim <gbenhaim@gmail.com>
2014-05-10 18:56:27 +0300

    implemented the builtin Logger interface

--- le.go
+++ le.go
@@ -4,73 +4,174 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"os"
 	"time"
 )
 
-type LE struct {
-	conn  net.Conn
-	token string
+type Logger struct {
+	conn   net.Conn
+	flags  int
+	prefix string
+	token  string
 }
 
-func Connect(token string) (*LE, error) {
-	le := LE{
+func Connect(token string) (*Logger, error) {
+	Logger := Logger{
 		token: token,
 	}
 
-	if err := le.reopenConnection(); err != nil {
+	if err := Logger.reopenConnection(); err != nil {
 		return nil, err
 	}
 
-	return &le, nil
+	return &Logger, nil
 }
 
-func (le *LE) Close() error {
-	if le.conn != nil {
-		return le.conn.Close()
+func (Logger *Logger) Close() error {
+	if Logger.conn != nil {
+		return Logger.conn.Close()
 	}
 
 	return nil
 }
 
-func (le *LE) reopenConnection() error {
+func (Logger *Logger) reopenConnection() error {
 	conn, err := net.Dial("tcp", "data.logentries.com:80")
 	if err != nil {
 		return err
 	}
 
-	le.conn = conn
+	Logger.conn = conn
 
 	return nil
 }
 
-func (le *LE) isOpenConnection() bool {
-	if le.conn == nil {
+func (Logger *Logger) isOpenConnection() bool {
+	if Logger.conn == nil {
 		return false
 	}
 
 	buf := make([]byte, 1)
 
-	le.conn.SetReadDeadline(time.Now())
+	Logger.conn.SetReadDeadline(time.Now())
 
-	if _, err := le.conn.Read(buf); err.(net.Error).Timeout() == true &&
+	if _, err := Logger.conn.Read(buf); err.(net.Error).Timeout() == true &&
 		err != io.EOF {
 
-		le.conn.SetReadDeadline(time.Time{})
+		Logger.conn.SetReadDeadline(time.Time{})
 
 		return true
 	} else {
-		le.conn.Close()
+		Logger.conn.Close()
 
 		return false
 	}
 }
 
-func (le *LE) Println(msg string) {
-	if !le.isOpenConnection() {
-		if err := le.reopenConnection(); err != nil {
-			return
+func (Logger *Logger) ensureOpenConnection() error {
+	if !Logger.isOpenConnection() {
+		if err := Logger.reopenConnection(); err != nil {
+			return err
 		}
 	}
 
-	fmt.Fprintln(le.conn, le.token, msg)
+	return nil
+}
+
+func (Logger *Logger) Fatal(v ...interface{}) {
+	if err := Logger.ensureOpenConnection(); err != nil {
+		return
+	}
+
+	fmt.Fprint(Logger.conn, Logger.token, Logger.prefix, v)
+	os.Exit(1)
+}
+
+func (Logger *Logger) Fatalf(v ...interface{}) {
+	if err := Logger.ensureOpenConnection(); err != nil {
+		return
+	}
+
+	fmt.Fprintf(Logger.conn, Logger.token, Logger.prefix, v)
+	os.Exit(1)
+}
+
+func (Logger *Logger) Fatalln(v ...interface{}) {
+	if err := Logger.ensureOpenConnection(); err != nil {
+		return
+	}
+
+	fmt.Fprintln(Logger.conn, Logger.token, Logger.prefix, v)
+	os.Exit(1)
+}
+
+func (Logger *Logger) Flags() int {
+	return Logger.flags
+}
+
+func (Logger *Logger) Output(calldepth int, s string) error {
+	return nil
+}
+
+func (Logger *Logger) Panic(v ...interface{}) {
+	if err := Logger.ensureOpenConnection(); err != nil {
+		return
+	}
+
+	fmt.Fprint(Logger.conn, Logger.token, Logger.prefix, fmt.Sprint(v...))
+	panic("")
+}
+
+func (Logger *Logger) Panicf(format string, v ...interface{}) {
+	if err := Logger.ensureOpenConnection(); err != nil {
+		return
+	}
+
+	fmt.Fprintf(Logger.conn, Logger.token, Logger.prefix, fmt.Sprintf(format, v...))
+	panic("")
+}
+
+func (Logger *Logger) Panicln(v ...interface{}) {
+	if err := Logger.ensureOpenConnection(); err != nil {
+		return
+	}
+
+	fmt.Fprintln(Logger.conn, Logger.token, Logger.prefix, fmt.Sprintln(v...))
+	panic("")
+}
+
+func (Logger *Logger) Prefix() string {
+	return Logger.prefix
+}
+
+func (Logger *Logger) Print(v ...interface{}) {
+	if err := Logger.ensureOpenConnection(); err != nil {
+		return
+	}
+
+	fmt.Fprint(Logger.conn, Logger.token, Logger.prefix, fmt.Sprint(v...))
+}
+
+func (Logger *Logger) Printf(format string, v ...interface{}) {
+	if err := Logger.ensureOpenConnection(); err != nil {
+		return
+	}
+
+	fmt.Fprintf(Logger.conn, Logger.token, Logger.prefix, fmt.Sprintf(format, v...))
+}
+
+func (Logger *Logger) Println(v ...interface{}) {
+	if err := Logger.ensureOpenConnection(); err != nil {
+		return
+	}
+
+	fmt.Fprintln(Logger.conn, Logger.token, Logger.prefix, fmt.Sprintln(v...))
+}
+
+func (Logger *Logger) SetFlags(flag int) {
+	Logger.flags = flag
+}
+
+func (Logger *Logger) SetPrefix(prefix string) {
+	Logger.prefix = prefix
 }
