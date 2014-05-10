a83d371e8c28a2ccd5e7be6f441cff23734847cf
Gal Ben-Haim <gbenhaim@gmail.com>
2014-05-10 16:24:35 +0300

    added reconnection on Println

--- le.go
+++ le.go
@@ -2,7 +2,9 @@ package logentries
 
 import (
 	"fmt"
+	"io"
 	"net"
+	"time"
 )
 
 type LE struct {
@@ -15,12 +17,9 @@ func Connect(token string) (*LE, error) {
 		Token: token,
 	}
 
-	conn, err := net.Dial("tcp", "data.logentries.com:80")
-	if err != nil {
+	if err := le.reopenConnection(); err != nil {
 		return nil, err
 	}
-
-	le.Conn = conn
 
 	return &le, nil
 }
@@ -33,9 +32,42 @@ func (le *LE) Close() error {
 	return nil
 }
 
-func (le *LE) Println(msg string) {
+func (le *LE) reopenConnection() error {
+	conn, err := net.Dial("tcp", "data.logentries.com:80")
+	if err != nil {
+		return err
+	}
+
+	le.Conn = conn
+
+	return nil
+}
+
+func (le *LE) isOpenConnection() bool {
 	if le.Conn == nil {
-		return
+		return false
+	}
+
+	buf := make([]byte, 1)
+
+	le.Conn.SetReadDeadline(time.Now())
+
+	if _, err := le.Conn.Read(buf); err == io.EOF {
+		le.Conn.Close()
+
+		return false
+	} else {
+		le.Conn.SetReadDeadline(time.Time{})
+
+		return true
+	}
+}
+
+func (le *LE) Println(msg string) {
+	if !le.isOpenConnection() {
+		if err := le.reopenConnection(); err != nil {
+			return
+		}
 	}
 
 	fmt.Fprintln(le.Conn, le.Token, msg)
